c1ddf72b1d806410e01a3559d4cb8264500769eb
Romeo Flauta <meoflauta@gmail.com>
2024-03-27 07:13:37 +0800

    Initial Commit

---
+++ Guidomia-SwiftUI/Guidomia_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guidomia_SwiftUIApp.swift
+//  Guidomia-SwiftUI
+//
+//  Created by Romeo Flauta on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Guidomia_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
