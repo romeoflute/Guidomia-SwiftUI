4f61f3233e98de3617548b7b703b2ba500e76d80
Romeo Flauta <meoflauta@gmail.com>
2024-03-28 15:51:26 +0800

    Add tests for checking coredata objects

--- Guidomia-SwiftUITests/Service/CoreDataServiceTests.swift
+++ Guidomia-SwiftUITests/Service/CoreDataServiceTests.swift
@@ -58,11 +58,67 @@ final class CoreDataServiceTests: XCTestCase {
     func test_fetchWrongEntity_resultsInError() async {
         sut = CoreDataService(container: container, entityName: "CDVan")
         do {
-            let coreDataCars = try await sut.loadCars()
+            let _ = try await sut.loadCars()
             XCTFail("Failed to load cars from CoreData")
         } catch {
             XCTAssertEqual(error.localizedDescription, DataError.unableToFetchFromCoreData.localizedDescription
             )
+        }
+    }
+    
+    func test_savingCarsInCoreData_savesAssociatedConsAndProsInOneToManyRelationship() {
+        let carCount = 4
+        let cars = CoreDataTestsUtil.shared.createCars(carCount)
+        
+        let fetchRequest = CDCar.fetchRequest()
+        let context = container.viewContext
+        
+        do {
+            try CoreDataService.saveCars(cars, context: sut.container.viewContext)
+            
+            let cdCars: [CDCar] = try context.fetch(fetchRequest)
+            
+            XCTAssertEqual(cdCars.count, 4)
+            XCTAssertEqual(cdCars.first?.consList?.count, 2)
+            XCTAssertEqual(cdCars.first?.prosList?.count, 2)
+        } catch {
+            XCTFail("Unable to save and fetch cars to and from Core Data")
+        }
+    }
+    
+    func test_savingCarsInCoreData_savesAssociatedConsObjects() {
+        let carCount = 4
+        let cars = CoreDataTestsUtil.shared.createCars(carCount)
+        
+        let fetchRequest = Con.fetchRequest()
+        let context = container.viewContext
+        
+        do {
+            try CoreDataService.saveCars(cars, context: sut.container.viewContext)
+            
+            let cons: [Con] = try context.fetch(fetchRequest)
+            
+            XCTAssertEqual(cons.count, 8)
+        } catch {
+            XCTFail("Unable to save cars and fetch cons to and from Core Data")
+        }
+    }
+    
+    func test_savingCarsInCoreData_savesAssociatedProsObjects() {
+        let carCount = 4
+        let cars = CoreDataTestsUtil.shared.createCars(carCount)
+        
+        let fetchRequest = Pro.fetchRequest()
+        let context = container.viewContext
+        
+        do {
+            try CoreDataService.saveCars(cars, context: sut.container.viewContext)
+            
+            let pros: [Pro] = try context.fetch(fetchRequest)
+            
+            XCTAssertEqual(pros.count, 8)
+        } catch {
+            XCTFail("Unable to save cars and fetch pros to and from Core Data")
         }
     }
 }
